f8971f39808ec37404afd2859181818ad5b71650
YXYang <160738188+YXYang960102@users.noreply.github.com>
2025-04-30 14:25:25 +0800

    Update Swerve Drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,21 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.LimelightConstants.Limelight;
+import frc.robot.Constants.OIConstants;
+import frc.robot.commands.SwerveFieldRelative;
+import frc.robot.subsystems.SwerveSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,16 +28,24 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final DriverTrain m_drive = new DriverTrain();
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private static CommandXboxController m_driverController = new CommandXboxController(
+      OIConstants.kDriverControllerPort);
+
+
+  // Create auto chooser
+  private final SendableChooser<Command> autoChooser;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
+    // configureNamedCommands();
+    autoChooser = AutoBuilder.buildAutoChooser(); // Default auto will be `Commands.none()`
+    SmartDashboard.putData("Auto Mode", autoChooser);
     configureBindings();
+    setDefaultCommand();
   }
 
   /**
@@ -39,7 +58,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+  
     
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
@@ -47,7 +66,13 @@ public class RobotContainer {
     
   }
 
-  
+  private void setDefaultCommand() {
+    swerveSubsystem.setDefaultCommand(new SwerveFieldRelative(swerveSubsystem,
+        () -> -m_driverController.getLeftY(), // X-Axis
+        () -> -m_driverController.getLeftX(), // Y-Axis
+        () -> -m_driverController.getRightX() // R-Axis
+    ));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
