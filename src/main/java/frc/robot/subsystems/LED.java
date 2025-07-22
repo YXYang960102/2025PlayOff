5c989b3b5a11b5f39c93038b2167b76cf3009f8c
YXYang <160738188+YXYang960102@users.noreply.github.com>
2025-07-23 00:43:49 +0800

    Update LED

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -5,10 +5,22 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
+
+import edu.wpi.first.units.Units;
+
+
+import frc.robot.Constants.LEDConstants;
+
+import java.util.Map;
 
 public class LED extends SubsystemBase {
   /** Creates a new LED. */
-  public LED() {}
+  public LED() {
+
+  }
 
   @Override
   public void periodic() {
